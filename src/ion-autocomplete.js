d7a42cfe37b3ecb1d059362b1c2e43a5eaadc18f
Rossana Bermudez <bermudez@new-frontiers.de>
2017-06-09 10:02:33 +0200

    Disabled autofocus

--- src/ion-autocomplete.js
+++ src/ion-autocomplete.js
@@ -317,9 +317,9 @@ angular.module('ion-autocomplete', []).directive('ionAutocomplete', [
 
                         // focus on the search input field
                         if (searchInputElement.length > 0) {
-                            searchInputElement[0].focus();
+                            //searchInputElement[0].focus();
                             setTimeout(function () {
-                                searchInputElement[0].focus();
+                                //searchInputElement[0].focus();
                             }, 100);
                         }
 
